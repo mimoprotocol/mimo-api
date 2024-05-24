f574f8e084f999220327c86001da73e515d94ee5
zongzheng <304389312@qq.com>
2024-05-24 09:48:05 +0800

    support cache in background and return stale value
    Signed-off-by: zongzheng <304389312@qq.com>

--- src/lib/cache.ts
+++ src/lib/cache.ts
@@ -3,5 +3,6 @@ import { caching } from 'cache-manager';
 export const getMemoryCache = async () =>  caching('memory', {
     max: 10000,
     ttl: 10 * 1000 /*milliseconds*/,
+    refreshThreshold: 3 * 1000
   });
   
