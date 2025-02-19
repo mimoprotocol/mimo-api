22ceba0de257f6262abb33f0595a641824672f42
Atom <929746602@qq.com>
2025-02-19 10:34:22 +0800

    fix: token list query

--- src/lib/token.ts
+++ src/lib/token.ts
@@ -1,53 +1,32 @@
-
 import { cache } from "../lib/cache";
-import { ethers } from "ethers";
-import { config } from "../config";
 
 export const getTokenFromPlatforms = async (platform: string) => {
-    const value = await cache.getOrSet(
-      `getTokenFromPlatforms-${platform}`,
-      async () => {
-        console.time('getTokenFromPlatforms')
-        let r = await fetch(config.ioPayGraphAPIURL, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-          },
-          body: JSON.stringify({
-            query: `{
-          token_list_v3(where: { platforms: { _has_key: "${platform}" } }, order_by: { market_cap: desc_nulls_last }, limit: 1000) {
-            id
-            logo
-            name
-            address:platforms(path: "${platform}")
-            symbol
-            current_price
-            market_cap
-            decimals(path:"${platform}")
-            is_depin_token
-            tags
-          }
-        }`,
-          }),
-        });
-        let data = await r.json();
-        const token_list_v3 = data.data.token_list_v3;
-        let arr: any [] = [];
-        token_list_v3.forEach((i: any) => {
-          if (
-            i.address &&
-            ethers.utils.isAddress(i.address) &&
-            i.address != "0x0000000000000000000000000000000000001010"
-          )
-            arr.push(i);
-        });
-        console.timeEnd('getTokenFromPlatforms')
-        return arr;
-      },
-       {
-        ttl: 10 * 1000
-       }
-    );
-    return value
-  };
+  const value = await cache.getOrSet(
+    `getTokenFromPlatforms-${platform}`,
+    async () => {
+      let r = await fetch("https://api.iopay.me/api/rest/token_list/iotex", {
+        method: "GET",
+      });
+      let data = await r.json();
+      return data.token_list_v4.map((item) => {
+        return {
+          id: item.id,
+          logo: item.logo,
+          name: item.name,
+          address: item.address,
+          symbol: item.symbol,
+          current_price: item.current_price,
+          market_cap: item.market_cap,
+          decimals: item.decimals,
+          is_depin_token: item.is_depin_token,
+          tags: item.tags,
+          logoURI: item.logo,
+        };
+      }).filter((item: any) => item.logo);
+    },
+    {
+      ttl: 10 * 1000,
+    }
+  );
+  return value;
+};
